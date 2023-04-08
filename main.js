2258cbf1c531fc8e861984cac47cac96c4ada444
Juan Peixoto <juanpeixoto1998@gmail.com>
2023-04-08 10:28:13 -0300

    turned slider to automatic

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
+function conectButtonToImage(button, buttonIndex, imagesHrefList)
+{
+    button.onclick = () =>
+    {
+        sliderBody.style.backgroundImage = `url("../images/${imagesHrefList[buttonIndex]}")`;
+    };
+}
+
+
+function sleep(ms)
+{
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+async function loopSlider(arrayOfButtons)
+{
+    for (let i = 0;i < arrayOfButtons.length;i++)
+    {
+        await sleep(4000);
+        arrayOfButtons[i].click();
+    }
+
+    loopSlider(arrayOfButtons);
+}
+
+
 const sliderBody = document.getElementById('sliderBody');
-
 const controlCircles = document.querySelectorAll('.control-circle');
-
 const imagesHref = [
     'bigworlds.webp',
     'futuristic-city.webp',
@@ -15,11 +40,4 @@ controlCircles.forEach((button, buttonIndex) =>
     button.addEventListener('click', conectButtonToImage(button, buttonIndex, imagesHref));
 });
 
-
-function conectButtonToImage(button, buttonIndex, imagesHrefList)
-{
-    button.onclick = () =>
-    {
-        sliderBody.style.backgroundImage = `url("../images/${imagesHrefList[buttonIndex]}")`;
-    };
-}
+loopSlider(controlCircles);
